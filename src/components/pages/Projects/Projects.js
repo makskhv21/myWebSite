0e57697eb18bbecdcf43ce42fa67213ab68c104d
repo <khvits21@gmail.com>
2024-10-10 15:45:24 +0300

    Refactoring footer on all pages

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
                     <div className="project">
                         <a href="/Projects/1"><img src={projectImage} alt="projectImage" className="project-image" /></a>
                         <h3 className="project-name">Project 1</h3>
-                        <p className="project-desc">Technology: React, Node.js</p>
+                        <p className="project-desc">Technology: React, Node.js React, Node.js React, Node.js</p>
                     </div>
                     <div className="project">
                          <a href="/Projects/2"><img src={projectImage} alt="projectImage" className="project-image" /></a>
@@ -21,6 +21,21 @@ function Projects() {
                     </div>
                     <div className="project">
                         <a href="/Projects/3"><img src={projectImage} alt="projectImage" className="project-image" /></a>
+                        <h3 className="project-name">Project 1</h3>
+                        <p className="project-desc">Technology: React, Node.js</p>
+                    </div>
+                    <div className="project">
+                        <a href="/Projects/4"><img src={projectImage} alt="projectImage" className="project-image" /></a>
+                        <h3 className="project-name">Project 1</h3>
+                        <p className="project-desc">Technology: React, Node.js</p>
+                    </div>
+                    <div className="project">
+                        <a href="/Projects/5"><img src={projectImage} alt="projectImage" className="project-image" /></a>
+                        <h3 className="project-name">Project 1</h3>
+                        <p className="project-desc">Technology: React, Node.js</p>
+                    </div>
+                    <div className="project">
+                        <a href="/Projects/6"><img src={projectImage} alt="projectImage" className="project-image" /></a>
                         <h3 className="project-name">Project 1</h3>
                         <p className="project-desc">Technology: React, Node.js</p>
                     </div>
