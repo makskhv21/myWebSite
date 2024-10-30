f41de86fe6908ea0910922ac80400adade45ea31
repo <khvits21@gmail.com>
2024-10-31 01:22:43 +0200

    fix: Change style and structure Skill comp

--- src/components/pages/Skills/Skills.js
+++ src/components/pages/Skills/Skills.js
@@ -20,28 +20,28 @@ function Skills() {
     return (
         <main className="main-skills">
             <h1 className="header-skills">Skills</h1>
-            <div className="skills">
-                <h2>Hard</h2>
-                <h2>Soft</h2>
-            </div>
             <div className="stars-polygon">
-                <div className="one-stars">
-                    {starWords.map((word, index) => (
-                        <StarItem key={index} word={word} />
-                    ))}
-                </div>
-                <div className="two-stars">
-                    {starWords.map((word, index) => (
-                        <StarItem key={index} word={word} />
-                    ))}
+                <div className="stars">
+                    <h2>Hard</h2>
+                    <div className="one-stars">
+                        {starWords.map((word, index) => (
+                            <StarItem key={index} word={word} />
+                        ))}
+                    </div>
+                    <div className="two-stars">
+                        {starWords.map((word, index) => (
+                            <StarItem key={index} word={word} />
+                        ))}
+                    </div>
                 </div>
                 <div className="polygon">
+                    <h2>Soft</h2>
                     {polygonWords.map((word, index) => (
                         <PolygonItem key={index} word={word} />
                     ))}
                 </div>
             </div>
-            <a target="_blank" href="https://github.com/makskhv21/makskhv21/blob/main/SKILLS.md" className="GithubSkills">Software engineering skills</a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/makskhv21/makskhv21/blob/main/SKILLS.md" className="GithubSkills">Software engineering skills</a>
         </main>
     );
 }
