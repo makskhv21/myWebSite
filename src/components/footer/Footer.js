c92cbaf0e0a38542c2b6ef69bdb889bdff44d1ad
repo <khvits21@gmail.com>
2024-10-30 22:59:31 +0200

    Add adaptivity for Home comp

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,19 +11,19 @@ function Footer() {
         <footer>
             <div className="footer-img">
                 <a href="mailto:m.khvyts.ser@gmail.com">
-                    <img src={emailPhoto} alt="email-photo"/>
+                    <img className="photo" src={emailPhoto} alt="email-photo"/>
                 </a>
                 <a href="https://github.com/makskhv21">
-                    <img src={githubPhoto} alt="github-photo"/>
+                    <img className="photo" src={githubPhoto} alt="github-photo"/>
                 </a>
                 <a href="tel:+number">
-                    <img src={phonePhoto} alt="phone-photo"/>
+                    <img className="photo" src={phonePhoto} alt="phone-photo"/>
                 </a>
                 <a href="https://www.linkedin.com/in/maksim-khvyts-5833b82b5/">
-                    <img src={linkedinPhoto} alt="linkedin-photo"/>
+                    <img className="photo" src={linkedinPhoto} alt="linkedin-photo"/>
                 </a>
                 <a href="https://maps.app.goo.gl/obSCA8fJpHGQYnxL9">
-                    <img src={locationPhoto} alt="location-photo"/>
+                    <img className="photo" src={locationPhoto} alt="location-photo"/>
                 </a>
             </div>
             <p className="footer-site">© 2024 mh-front-dev.com</p>
