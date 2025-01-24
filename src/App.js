2923d43fb9b4103d73d1497691ed6bd07cf3d152
repo <khvits21@gmail.com>
2025-01-24 15:27:43 +0200

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import "./App.css"
-
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/navbar/Navbar"
-import Main from "./components/pages/Main/Main"
-import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar";
+import Main from "./components/pages/Main/Main";
+import Footer from "./components/footer/Footer";
 
 /* pages */
-import Skills from "./components/pages/Skills/Skills"
-import Contacts from "./components/pages/Contacts/Contacts"
-import Projects from "./components/pages/Projects/Projects"
-import Project from "./components/pages/Project"
+import Skills from "./components/pages/Skills/Skills";
+import Contacts from "./components/pages/Contacts/Contacts";
+import Projects from "./components/pages/Projects/Projects";
+import Project from "./components/pages/Project";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        < Navbar />
+      <Router basename="/myWebSite"> 
+        <Navbar />
         <Routes>
-            <Route path="/" element={<Main /> }/>
-            <Route path="/Home" element={<Main /> }/>
-            <Route path="/Projects" element={<Projects />}/>
-            <Route path="/Projects/:id" element={<Project />}/>
-            <Route path="/Skills" element={<Skills />}/>
-            <Route path="/Contacts" element={<Contacts /> }/>
+          <Route path="/" element={<Main />} />
+          <Route path="/Home" element={<Main />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Projects/:id" element={<Project />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Contacts" element={<Contacts />} />
         </Routes>
-        < Footer />
+        <Footer />
       </Router>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -24,7 +24,7 @@ function Main() {
                     <p className="info-desription custom-font">I am a frontend developer passionate about creating intuitive and efficient interfaces.</p>
                 </div>
                 <div className="button-container">
-                    <button className="btn-Download"><a src='#'></a>Download CV</button>
+                    <button className="btn-Download"><a href='#'></a>Download CV</button>
                     <button onClick={openModal} className="btn-About">About Me</button>
                     <Modal isOpen={isModalOpen} onClose={closeModal} />
                 </div>
